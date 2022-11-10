fc5c6a12a56d7f4f1c5fd2db320a07639f7a2d03
Max10001 <maxim.babi4ev2015@yandex.ru>
2022-11-10 21:42:54 +0300

    2 lesson

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ def get_data():
 
     response = requests.post('https://www.mvideo.ru/bff/product-details/list', cookies=cookies, headers=headers,
                              json=json_data).json()
-    with open('2 items.json', 'w') as file:
+    with open('2_items.json', 'w') as file:
         json.dump(response, file, indent=4, ensure_ascii=False)
 #    print(len(response.get('body').get('products')))
     products_ids_str = ','.join(products_ids)
@@ -181,8 +181,50 @@ def get_data():
     with open('3_prices.json', 'w') as file:
         json.dump(response, file, indent=4, ensure_ascii=False)
 
+    items_prices = {}
+
+    material_prices = response.get('body').get('materialPrices')
+
+    for item in material_prices:
+        item_id = item.get('price').get('productId')
+        item_base_price = item.get('price').get('basePrice')
+        item_sale_price = item.get('price').get('salePrice')
+        item_bonus = item.get('bonusRubles').get('total')
+
+        items_prices[item_id] = {
+            'item_basePrice': item_base_price,
+            'item_salePrice': item_sale_price,
+            'item_bonus': item_bonus
+        }
+
+    with open('4_items_prices.json', 'w') as file:
+        json.dump(items_prices, file, indent=4, ensure_ascii=False)
+
+def get_result():
+    with open('2_items.json') as file:
+        products_data = json.load(file)
+
+    with open('4_items_prices.json') as file:
+        products_prices = json.load(file)
+
+    products_data = products_data.get('body').get('products')
+
+    for item in products_data:
+        product_id = item.get('productId')
+
+        if product_id in products_prices:
+            prices = products_prices[product_id]
+
+        item['item_basePrice'] = prices.get('item_basePrice')
+        item['item_salePrice'] = prices.get('item_salePrice')
+        item['item_bonus'] = prices.get('item_bonus')
+
+    with open('5_result.json', 'w') as file:
+        json.dump(products_data, file, indent=4, ensure_ascii=False)
+
 def main():
     get_data()
+    get_result()
 
 
 if __name__ == '__main__':
